c085669a12eaec337237e5691bae7b0c1d40e19b
00hello <445986+00hello@users.noreply.github.com>
2025-09-04 20:57:16 -0400

    First baml functions

--- 2025-09-03-basic-baml/main.py
+++ 2025-09-03-basic-baml/main.py
@@ -1,6 +1,52 @@
-def main():
-    print("Hello from 2025-09-03-basic-baml!")
+from baml_client.sync_client import b
+from baml_client.types import Resume
+import os
 
+def example(raw_resume: str) -> Resume: 
+  # BAML's internal parser guarantees ExtractResume
+  # to be always return a Resume type
+  response = b.ExtractResume(raw_resume)
+  return response
+
+def example_stream(raw_resume: str) -> Resume:
+  stream = b.stream.ExtractResume(raw_resume)
+  for msg in stream:
+    print(msg) # This will be a PartialResume type
+  
+  # This will be a Resume type
+  final = stream.get_final_response()
+
+  return final
+
+
+
+def main():
+    # text = "Alice Smith is 25 years old and works at alice@company.com"
+    
+    # try:
+    #     person = b.ExtractPerson(text)
+    #     print(f"Name: {person.name}")
+    #     print(f"Age: {person.age}")
+    #     print(f"Email: {person.email}")
+    # except Exception as e:
+    #     print(f"Error: {e}")
+
+
+    example("""
+        Vaibhav Gupta
+      vbv@boundaryml.com
+
+      Experience:
+      - Founder at BoundaryML
+      - CV Engineer at GCP
+      - CV Engineer at XBOX
+
+      Skills:
+      - Rust
+      - C++
+         """)
 
 if __name__ == "__main__":
     main()
+
+
